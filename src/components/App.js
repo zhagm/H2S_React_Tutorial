1ab3f31633f70094144c4e9bcd1e26fb9fe20075
zhagm <zhagdev@gmail.com>
2019-05-04 07:56:38 -0700

    ex01, section Creating a Table Component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,28 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello, World!</h1>
-        )
+            <React.Fragment>
+                <h1>To-Do List</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                            <th>Completed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Complete the NodeJS course</td>
+                            <td>true</td>
+                        </tr>
+                        <tr>
+                            <td>Complete Ex.01 of the React Course</td>
+                            <td>false</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
     }
 }
 
