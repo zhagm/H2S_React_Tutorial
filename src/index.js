f1f16676ee67811eb617d18bdb026adb40c214cb
zhagm <zhagdev@gmail.com>
2019-05-04 07:14:40 -0700

    ex01, exporting hello world into App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const App = <h1>Hello, World!</h1>
-
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
